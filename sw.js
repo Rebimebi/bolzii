c401a04c24c2a2320a04b1f12e6a5a7b37313d71
Rebimebi <121923935+Rebimebi@users.noreply.github.com>
2026-07-03 02:37:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Хоёулаа Болзий — Service Worker */
-const CACHE = 'bolzii-v108';
+const CACHE = 'bolzii-v109';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
